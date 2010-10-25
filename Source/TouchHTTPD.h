1688333e6648e6a5a26e1752c53ab164609f948a
Aleksejs Mjaliks <am@keeper.lv>
2010-10-26 05:42:28 +0800

    Header file with list of imports

---
+++ Source/TouchHTTPD.h
@@ -0,0 +1,11 @@
+/**
+ List of all header files in TouchHTTPD. Use for easy import of all TouchHTTPD classes.
+ 
+ P.S. It isn't complete yet. Add missing files, if required.
+ **/
+
+#import "CHTTPServer.h"
+#import "CHTTPMessage.h"
+#import "CHTTPMessage_ConvenienceExtensions.h"
+#import "CWebDAVHTTPHandler.h"
+#import "TouchHTTPDConstants.h"
