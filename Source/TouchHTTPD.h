da6620bdbaa2c194bd7c97eb7c331e263a8cb51e
Jonathan Wight <jwight@mac.com>
2011-07-11 13:28:24 -0700

    Fixed header copyright.

--- Source/TouchHTTPD.h
+++ Source/TouchHTTPD.h
@@ -1,6 +1,15 @@
+//
+//  TouchHTTPD.h
+//  TouchHTTPD
+//
+//  Created by Jonathan Wight on 1/1/2000.
+//  Copyright 2011 toxicsoftware.com. All rights reserved.
+//
+
+
 /**
  List of all header files in TouchHTTPD. Use for easy import of all TouchHTTPD classes.
- 
+
  P.S. It isn't complete yet. Add missing files, if required.
  **/
 
